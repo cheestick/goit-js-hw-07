4564895105dfdf9b41ef90659efc1acf3df0683d
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-02-13 20:19:44 +0200

    02-lightbox completed

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,6 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const galleryRef = document.querySelector(".gallery");
+galleryRef.addEventListener("click", imageClickHandler);
 
 (function renderGallery() {
   galleryRef.insertAdjacentHTML(
@@ -9,21 +10,25 @@ const galleryRef = document.querySelector(".gallery");
     galleryItems
       .map(
         ({ preview, original, description }, index) =>
-          `<a class="gallery__item" href="${original}}">
-                        <img class="gallery__image" src="${preview}" alt="${description}" />
+          `<a class="gallery__item" href="${original}">
+                        <img class="gallery__image" src="${preview}" alt="${description}"  />
                     </a>`
       )
       .join("")
   );
 })();
 
-galleryRef.addEventListener("click", imageClickHandler);
+const options = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+const gallery = new SimpleLightbox(".gallery a", options);
 
 function imageClickHandler(event) {
   const element = event.target;
   if (element.nodeName !== "IMG") return;
 
   event.preventDefault();
-
-  //   showOriginalImageModal(element);
+  gallery.open();
 }
