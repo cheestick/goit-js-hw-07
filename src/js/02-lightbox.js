068266716632c03826331e295147f3e8b3d0036a
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2022-02-13 17:41:09 +0200

    02-lightbox render gallery added

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+(function renderGallery() {
+  galleryRef.insertAdjacentHTML(
+    "afterbegin",
+    galleryItems
+      .map(
+        ({ preview, original, description }, index) =>
+          `<a class="gallery__item" href="${original}}">
+                        <img class="gallery__image" src="${preview}" alt="${description}" />
+                    </a>`
+      )
+      .join("")
+  );
+})();
+
+galleryRef.addEventListener("click", imageClickHandler);
+
+function imageClickHandler(event) {
+  const element = event.target;
+  if (element.nodeName !== "IMG") return;
+
+  event.preventDefault();
+
+  //   showOriginalImageModal(element);
+}
